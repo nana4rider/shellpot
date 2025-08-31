0569ccb41d25dbae3047d6eda9c394871177c219
nana4rider <register.develop@nana4.net>
2025-08-31 18:14:16 +0900

    exclude prometheus

--- backup-home.sh
+++ backup-home.sh
@@ -11,7 +11,7 @@ trap catch ERR
 SOURCE_DIR=$HOME
 LOCAL_TMP="/tmp/backup-home.tar.gz"
 MAX_BACKUPS=15
-EXCLUDE_DIRS=("**/node_modules" "**/snapshot/*/*.jpg" ".cache" ".docker")
+EXCLUDE_DIRS=("**/node_modules" "**/snapshot/*/*.jpg" ".cache" ".docker" "data/prometheus")
 
 source ~/config/common/storage.env
 
